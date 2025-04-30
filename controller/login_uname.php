8175b95d7874bd3277367779efac70b925e8fe74
TQBao <135828322+TQBao392@users.noreply.github.com>
2025-05-01 02:25:32 +0700

    Update login_uname.php

--- controller/login_uname.php
+++ controller/login_uname.php
@@ -1,11 +1,12 @@
 <?php
     // session_start();
+    $id = $_SESSION['id'];
     $role=$_SESSION['role'];
     if($role==1) {
       header ('Location: adminhienthi.php');
     }
     elseif ($role==0) {
-        header ('Location: tranghienthi.php?quanly=trangchu');
+        header ("Location: tranghienthi.php?quanly=quanlytaikhoan&id=$id");
     }
     
 ?>
